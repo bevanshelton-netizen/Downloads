2209f8b415cb5025938a018471454d3d6de55fca
bevanshelton-netizen <bevanshelton@gmail.com>
2026-09-23 21:43:22 +0200

    Remove Learner Driver SA from KORA crawler surface

--- kora-network/app/robots.ts
+++ kora-network/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: ['/', '/watch', '/live', '/kids', '/creators', '/advertise', '/learner-driver', '/legal/'],
+      allow: ['/', '/watch', '/live', '/kids', '/creators', '/advertise', '/legal/'],
       disallow: ['/admin/', '/account/', '/studio/', '/advertiser/', '/api/'],
     },
     sitemap: `${base}/sitemap.xml`,
